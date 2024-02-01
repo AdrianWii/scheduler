2311edc2d7f2ca92ef03e46e8075d0022e60c209
AdrianWii <awidlak@adrianwii.pl>
2024-02-01 14:07:57 +0100

    Show the upcoming meetings, for a given person

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,13 +2,17 @@ import { Controller, Get, Post, Delete, Body, Param } from '@nestjs/common';
 import { CreateUpdateUserDto } from './dto/create-update-user.dto';
 import { User } from './user.entity';
 import { UserService } from './user.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Meeting } from 'src/meeting/meeting.entity';
+import { MeetingService } from 'src/meeting/meeting.service';
 
 @Controller('user')
 @ApiTags("User")
 export class UserController {
 
-    constructor(private readonly userService: UserService) { }
+    constructor(
+        private readonly userService: UserService,
+    ) { }
 
     @Post()
     create(@Body() createUserDto: CreateUpdateUserDto): Promise<User> {
@@ -28,5 +32,12 @@ export class UserController {
     @Delete(':id')
     remove(@Param('id') id: string) {
         return this.userService.remove(id);
+    }
+
+    @Get('schedule/:id')
+    @ApiResponse({ status: 404, description: 'User with given ID not found' })
+    @ApiResponse({ status: 200, description: 'Meetings for user listed', type: Meeting })
+    async getUpcomingMeetingsForPersonById(@Param('id') id: string): Promise<Meeting[]> {
+        return await this.userService.getUpcomingMeetingsByUserId(id);
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,12 +3,15 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm/repository/Repository';
 import { User } from './user.entity';
 import { CreateUpdateUserDto } from './dto/create-update-user.dto';
+import { Meeting } from 'src/meeting/meeting.entity';
 
 @Injectable()
 export class UserService {
     constructor(
         @InjectRepository(User)
         private readonly userRepository: Repository<User>,
+        @InjectRepository(Meeting)
+        private readonly meetingRepository: Repository<Meeting>,
     ) { }
 
     async create(createUserDto: CreateUpdateUserDto): Promise<User> {
@@ -35,5 +38,22 @@ export class UserService {
             throw new NotFoundException(`The user "${id}" was not found`);
         }
         return { message: 'User successfully deleted' };
+    }
+
+    async getUpcomingMeetingsByUserId(id: string): Promise<Meeting[]> {
+        const user = await this.userRepository.findOne({ where: { id: id } });
+
+        if (!user) {
+            throw new NotFoundException(`User with ID ${id} not found`);
+        }
+
+        const now = new Date();
+        return await this.meetingRepository
+            .createQueryBuilder('meeting')
+            .innerJoin('meeting.participants', 'participant')
+            .where('participant.id = :id', { id })
+            .andWhere('meeting.startTime > :now', { now })
+            .orderBy('meeting.startTime', 'ASC')
+            .getMany();
     }
 }
